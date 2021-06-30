e3bc9250bd063c19df70df8d8954d81ca27ce7ff
Bruno Lebrao <brunolebrao@pop-os.localdomain>
2021-06-30 13:50:20 -0300

    add folder types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,22 +6,20 @@ import { theme } from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>Boilerplate</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <meta name="theme-color" content="#06092B" />
-          <meta
-            name="description"
-            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-          />
-        </Head>
-        <Component {...pageProps} />
-        <GlobalStyle />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 export default App
