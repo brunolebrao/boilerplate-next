7f65a72e6c6e7dbe4075f742f1733cfe90cb445e
Bruno Lebrao <brunolebrao@pop-os.localdomain>
2021-06-30 16:08:04 -0300

    change jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/index.ts',
+    '!src/styles/**'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
